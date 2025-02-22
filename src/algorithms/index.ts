0c08245a3d312662b5301beefa0ee06a8ea2808f
Dan Dyar <dan@parameterz.com>
2025-02-21 17:12:19 -0800

    adds BSE algorithms

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -1,12 +1,12 @@
 // src/algorithms/index.ts - Combine all algorithms
 import { Algorithm } from '../types/algorithm';
 import ase2016Algorithm from './ASE2016';
-//import bseAlgorithm from './BSE';
+import bseAlgorithm from './BSE2024';
 
 // Export all algorithms as a collection
 const algorithms: Record<string, Algorithm> = {
   ase2016: ase2016Algorithm,
-  //bse: bseAlgorithm,
+  bse: bseAlgorithm,
   // Add more algorithms as needed
 };
 
